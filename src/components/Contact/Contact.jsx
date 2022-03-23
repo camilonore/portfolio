91cc642b1d8fa1b5b2424ebb6d85b2e165339168
Camilo292 <caminore22@gmail.com>
2022-03-23 12:16:37 -0500

    send Email

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import './Contact.css';
 
 const Contact = () => {
+  const url = 'mailto:caminore22@gmail.com';
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    const subject = evt.target[0].value;
+    const body = evt.target[1].value;
+    window.open(`${url}?subject=${subject}&&body=${body}`);
+  }
   return (
     <footer className='contact' id='contact'>
       <h2>Contact</h2>
       <p>Do you want to know more about my work?</p>
-      <form action='' method='post' className='contact-form'>
-        <label htmlFor='email'>Your email:</label>
+      <form onSubmit={handleSubmit} className='contact-form'>
+        <label htmlFor='subject'>Subject:</label>
         <input
           type='text'
-          name='email'
-          id='email'
-          placeholder='example@gmail.com'
+          name='subject'
+          id='subject'
+          placeholder='I will hire you'
         />
         <label htmlFor='message'>Your message:</label>
         <textarea
