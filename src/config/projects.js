83cbe7cb4f76780bd285aa4b034903bd3ee51f8c
Camilo292 <caminore22@gmail.com>
2022-03-23 15:18:22 -0500

    Optimized pictures

--- src/config/projects.js
+++ src/config/projects.js
@@ -1,27 +1,27 @@
 export default projects = [
   {
-    imgUrl: '/nextCalendar.png',
+    imgUrl: '/nextCalendar.jpg',
     title: 'Next Calendar',
     description: 'Calendar made in NextJs, MongoDb, and pure css',
     repository: 'https://github.com/camilonore/next-calendar',
     website: 'https://next-calendar-one.vercel.app/',
   },
   {
-    imgUrl: '/rickAndMorty.png',
+    imgUrl: '/rickAndMorty.jpg',
     title: 'Rick and Morty',
     description: 'Rick and Morty website using React, and consuming the API',
     repository: 'https://github.com/camilonore/rick-and-morty-api',
     website: 'https://rick-and-morty-api-mu.vercel.app/',
   },
   {
-    imgUrl: '/calculator.png',
+    imgUrl: '/calculator.jpg',
     title: 'Calculator',
     description: 'Basic calculator made in React with Vite',
     repository: 'https://github.com/camilonore/ReactCalcutator',
     website: 'https://react-calcutator.vercel.app/',
   },
   {
-    imgUrl: '/reactIot.png',
+    imgUrl: '/reactIot.jpg',
     title: 'React IOT',
     description:
       'Basic iot project made in React, communication betwen the website and an ESP8266 programmed in arduino, for the communication i used firebase',
@@ -29,7 +29,7 @@ export default projects = [
     website: 'https://iot-basic-project.vercel.app/',
   },
   {
-    imgUrl: '/moviesApi.png',
+    imgUrl: '/moviesApi.jpg',
     title: 'Movies Api',
     description: 'Restful Api made in Nodejs, Express, MongoDb and TDD',
     repository: 'https://github.com/camilonore/movies-api',
