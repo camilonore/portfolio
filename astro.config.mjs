aedeaaa43a7872675acd130183bf8f1bb738cf2f
Camilo292 <caminore22@gmail.com>
2022-10-08 15:45:41 -0500

    change in case

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://camilonore.github.io',
-	base: '/Portfolio',
+	base: '/portfolio',
 	renderers: ['@astrojs/renderer-react'],
 });
